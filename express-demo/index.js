9d770a267ee4bf0b3bbe206a42276ffec8ea447b
Charikleia Niouraki <niouraki@gmail.com>
2018-11-09 14:31:42 +0200

    complete restful api express lesson

--- express-demo/index.js
+++ express-demo/index.js
@@ -31,7 +31,7 @@ app.get('/api/courses', function(req, res) {
 // :id is a parameter, I can also have multiple parameters like /api/posts/:year/:month
 app.get('/api/courses/:id', function(req, res) {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course was not found'); // 404 doesn't exist
+  if (!course) return res.status(404).send('The course was not found'); // 404 doesn't exist
   res.send(course);
   // req.query is for query parameters
 });
@@ -40,15 +40,8 @@ app.get('/api/courses/:id', function(req, res) {
 // first I give an endpoint, I am going to post my new course to the collection of courses
 // to check http services I use the extension POSTMAN
 app.post('/api/courses', function(req, res) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  const result = validateCourse(req.body);
+  if (result.error) return res.status(400).send(result.error.details[0].message);
 
   // if (!req.body.name || req.body.name.length < 3) {
     // 400 Bad request
@@ -63,6 +56,46 @@ app.post('/api/courses', function(req, res) {
   courses.push(course);
   res.send(course);
 });
+
+// to update a course
+app.put('/api/courses/:id', function(req, res) {
+  // look up the course with the id
+  // if not existing return 404
+  // validate
+  // if invalid return 400 Bad request
+  // if all ok update and return updated course to client
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course was not found');
+
+  const result = validateCourse(req.body);
+
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+// to delete a course
+app.delete('/api/courses/:id', function(req, res) {
+  // first look up the course
+  // if not existing return 404
+  // if it exists delete it and return the deleted course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course was not found');
+  // giet the index of the course I want to delete
+  const index = courses.indexOf(course);
+  // use the splice method to remove it from array
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+// keep the validation logic in one place so I can reuse it
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(course, schema);
+}
 // this callback is optional and it will be called once the app starts listening to the given port
 // it is better not to hardcode the port because I don't know if it will be available
 // app.listen(3000, function() {
